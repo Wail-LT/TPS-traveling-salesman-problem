9326b43001061ac7bb940a6362efb8fd307503e8
sanaa <32572661+SanaaD@users.noreply.github.com>
2020-02-22 23:01:43 +0100

    seconde

--- PTS/API/Result/Setup/GetToken.cs
+++ PTS/API/Result/Setup/GetToken.cs
@@ -8,6 +8,13 @@ namespace PTS.API.Result.Setup
     public class GetToken
     {
         public readonly int token;
-        public List<Tuple<string, double>>
+        public readonly List<Tuple<string, double>> selectionMethodes;
+        public readonly List<Tuple<string, string>> cityList;
+        public GetToken(int token, List<Tuple<string, double>> selectionMethodes, List<Tuple<string, string>> cityList)
+        {
+            this.token = token;
+            this.selectionMethodes = new List<Tuple<string, double>>(selectionMethodes);
+            this.cityList = new List<Tuple<string, string>>(cityList);
+        }
     }
 }
