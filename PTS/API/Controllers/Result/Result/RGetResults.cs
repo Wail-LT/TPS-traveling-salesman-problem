4f6ef8df593a49b4c24039ae3fbf877c559e3bd4
khdj <32709704+khdj@users.noreply.github.com>
2020-03-19 06:50:53 +0100

    Add ResultController
    - Add Result.controller.cs
    - Add RGetResukls.cs

---
+++ PTS/API/Controllers/Result/Result/RGetResults.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Serialization;
+using System.Threading.Tasks;
+
+namespace PTS.API.Result.Result
+{
+    [DataContract]
+    public class RGetResults
+    {
+        [DataMember]
+        public readonly Dictionary<string, string> cities;
+        [DataMember]
+        public readonly List<Tuple<string, double>> selectionMethods;
+        public RGetResults(int token, List<Tuple<string, double>> selectionMethodes, List<Tuple<string, string>> cityList)
+        {
+            this.cities = new Dictionary<string, string>(cities);
+            this.selectionMethods = new List<Tuple<string, double>>(selectionMethodes);
+        }
+    }
+}
